1ae080ab66d317f799d5c2e9a9ae48d6d3e9d3a5
deepsalunkhee <deepsalunkhee@gmail.com>
2024-12-13 16:54:10 +0530

    client deployment attemp 1.5

--- app/client/src/pages/dashboard.jsx
+++ app/client/src/pages/dashboard.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import Navbar from "../components/navbar";
-import Addjoin from "../components/addjoin";
+import Navbar from "../components/navbar.jsx";
+import Addjoin from "../components/addjoin.jsx";
 
-const dashboard = ({baseip}) => {
-    return (
-        <div className="min-h-[80vh] ">
-            <Navbar/>
-            <Addjoin baseip={baseip}/>
-        </div>
-    )
-}
+const dashboard = ({ baseip }) => {
+  return (
+    <div className="min-h-[80vh] ">
+      <Navbar />
+      <Addjoin baseip={baseip} />
+    </div>
+  );
+};
 
-export default dashboard
+export default dashboard;
