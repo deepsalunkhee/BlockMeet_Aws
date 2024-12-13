53018ae74f8a1cbfd50fb7cef348085579373ae7
deepsalunkhee <deepsalunkhee@gmail.com>
2024-12-13 16:27:29 +0530

    client deployment attemp 1.1

--- app/client/vite.config.js
+++ app/client/vite.config.js
@@ -6,18 +6,6 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   server: {
-    https: {
-      key: fs.readFileSync(
-        path.resolve(__dirname, "E:/MERN/Projests/FInlaYearProject/server.key"),
-      ),
-      cert: fs.readFileSync(
-        path.resolve(
-          __dirname,
-          "E:/MERN/Projests/FInlaYearProject/server.cert",
-        ),
-      ),
-    },
-    host: "0.0.0.0", // Expose to local network
     port: 5173, // Use the port of your choice
   },
 });
