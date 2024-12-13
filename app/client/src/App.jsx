5557a0ad77363122953c19731c75cbc4b9d62c94
deepsalunkhee <deepsalunkhee@gmail.com>
2024-12-13 17:17:33 +0530

    client deployment attemp 1.8

--- app/client/src/App.jsx
+++ app/client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Signin from "./pages/signin.jsx";
-import Signup from "./pages/Signup.jsx";
+import Signup from "./pages/signup.jsx";
 import Dashboard from "./pages/dashboard.jsx";
 import Conference from "./pages/conference11.jsx";
 import IpfsFiles from "./pages/ipfsFiles.jsx";
